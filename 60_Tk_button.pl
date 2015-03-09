b5ba1e72f6fa31845cea26e0321a5db1c2073992
Po_Hsun <showoowohs@google.com>
2015-03-09 15:16:33 -0600

    modify ./60_Tk_button.pl, click button can call mehod or function

--- 60_Tk_button.pl
+++ 60_Tk_button.pl
@@ -1,7 +1,11 @@
 #!/usr/bin/perl -w
 
+sub po_exit{
+	print "call po_exit()\n";
+	exit
+}
 use Tk;
 my $mw = new MainWindow; 
 my $label = $mw -> Label(-text=>"Button Test!!") -> pack();
-my $button = $mw -> Button(-text => "Quit", -command => sub { exit }) -> pack();
+my $button = $mw -> Button(-text => "Quit", -command => sub { po_exit }) -> pack();
 MainLoop;
