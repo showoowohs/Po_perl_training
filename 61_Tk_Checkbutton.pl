0fd9da7a45d61843b4d7bcd2c90d7c50ddfb5024
Po_Hsun <showoowohs@google.com>
2015-03-09 18:41:33 -0600

    modify ./61_Tk_Checkbutton.pl, traning 調整 button 位置

--- 61_Tk_Checkbutton.pl
+++ 61_Tk_Checkbutton.pl
@@ -6,8 +6,9 @@ my $mw = MainWindow->new;
 $mw->title("Test Window");
 $mw->Label(-text => "This is an Checkbutton example")->pack;
 
-$mw->Checkbutton(-text => "I like it!")->pack;
-$mw->Checkbutton(-text => "I hate it!")->pack;
-$mw->Checkbutton(-text => "I don't care")->pack;
 $mw->Button(-text => "Exit", -command => sub { exit })->pack(-side => 'bottom', -expand => 1, -fill => 'x');
+$mw->Checkbutton(-text => "I like it!")->pack(-side => 'left', -expand => 1);
+$mw->Checkbutton(-text => "I hate it!")->pack(-side => 'right', -expand => 1);
+$mw->Checkbutton(-text => "I don't care")->pack(-side => 'bottom', -expand => 1);
+
 MainLoop;
